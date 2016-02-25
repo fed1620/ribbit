6c006204b9d47d157f50473f3b45134e45afe33b
Fed <fedffm@gmail.com>
2016-02-25 16:49:35 -0700

    Began initial work on similarity algorithm

--- app/src/main/java/fedffm/bytewrite/Identifier.java
+++ app/src/main/java/fedffm/bytewrite/Identifier.java
@@ -27,6 +27,35 @@ public class Identifier {
     }
 
     /**
+     * Walk through each of the bitmaps and see which pixels match, versus which do not.
+     * @param sampleCharacter The bitmap for a known character from our CharacterBase
+     * @param unknownCharacterScaled The bitmap for the character we are trying to identify. This
+     *                               must be scaled to the exact same size as the bitmap of the
+     *                               sample character
+     * @return A percentage representing how similar the bitmaps are.
+     */
+    private static float computeSimilarity(Bitmap sampleCharacter, Bitmap unknownCharacterScaled) {
+        // Ensure that the two bitmaps are the exact same size. This is critical
+        assert (sampleCharacter.getWidth()  == unknownCharacterScaled.getWidth());
+        assert (sampleCharacter.getHeight() == unknownCharacterScaled.getHeight());
+        int width  = sampleCharacter.getWidth();
+        int height = sampleCharacter.getHeight();
+
+        // Store how many pixels there are and how many matching pixels there are
+        int pixels = width * height;
+        int matchingPixels = 0;
+
+        for (int x = 0; x < width; ++x)
+            for (int y = 0; y < height; ++y) {
+                if (sampleCharacter.getPixel(x, y) == unknownCharacterScaled.getPixel(x, y))
+                    matchingPixels++;
+            }
+        Log.i(LOG_TAG, "Out of " + pixels + " pixels, " + matchingPixels + " are a match");
+        Log.i(LOG_TAG, "Similarity: " + ((float)matchingPixels / (float)pixels));
+        return ((float)matchingPixels / (float)pixels);
+    }
+
+    /**
      * Identify a character
      * @param unknown The character to be identified
      * @return The character updated with a name and ASCII code
@@ -34,12 +63,24 @@ public class Identifier {
     public static Character identify(Character unknown, Context context) {
         // Start by getting the entire character base
         List<Character> characters = CharacterBase.getInstance(context).getAllCharacterSamples();
-        Bitmap scaled = null;
+        Bitmap unknownScaled;
+
+        // Use these to find the greatest similarity
+        float similarity         = (float)0.0;
+        float greatestSimilarity = (float)0.0;
 
         // Compare against each of them
         for (int i = 0; i < characters.size(); ++i) {
-            scaled = matchSize(characters.get(i).getBitmap(), unknown.getBitmap());
+            unknownScaled = matchSize(characters.get(i).getBitmap(), unknown.getBitmap());
+            similarity = computeSimilarity(characters.get(i).getBitmap(), unknownScaled);
+
+            // Which of the two character bitmaps are most similar?
+            if (similarity > greatestSimilarity)
+                greatestSimilarity = similarity;
         }
+
+        //TODO: This returns the very last similarity score, not the greatest...
+        Log.i(LOG_TAG, "Greatest similarity: " + (similarity * 100.00) + "%");
         return new Character();
     }
 
